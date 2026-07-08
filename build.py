7d72129fc43b1a042c3cc28da319d0da1bcac2ff
lukelin7429 <lukelin7429@users.noreply.github.com>
2026-07-09 07:21:40 +0800

    Add 2026 kids camp videos: drama rehearsal + morning chanting

--- build.py
+++ build.py
@@ -932,6 +932,8 @@ KIDS26_VIDS=[
  ("jZdGZISonZg","開營典禮"),
  ("-8UZ9gvvau0","相見歡"),
  ("KtCXndwWf9o","認識三寶"),
+ ("FHQVkDrRSLw","戲劇編排"),
+ ("tF8vhLjRJns","早課"),
 ]
 KIDS26_PLAYLIST="https://www.youtube.com/playlist?list=PLVFEzL1YVNqA"
 
