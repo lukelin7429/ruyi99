38ec9a983576e5cc43d11f42c74ac89bffbe3b74
lukelin7429 <lukelin7429@users.noreply.github.com>
2026-07-24 18:04:50 +0800

    新增瑜伽師地論卷62導讀：開啟三摩呬多地（定地）決擇
    十七種障定隨煩惱總辨，及隨身念經、摩訶俱瑟祉羅經、
    法因緣經、眠經四部經典引釋；史料在別廣諸相處中斷，
    頁面誠實標註。

--- build.py
+++ build.py
@@ -150,6 +150,7 @@ HANDMADE_PAGES=[
     "/study-group/yogacara-bhumi/vol-059/",
     "/study-group/yogacara-bhumi/vol-060/",
     "/study-group/yogacara-bhumi/vol-061/",
+    "/study-group/yogacara-bhumi/vol-062/",
     "/study-group/vimalakirti/",
     "/study-group/vimalakirti/pin01/",
     "/study-group/vimalakirti/pin02/",
