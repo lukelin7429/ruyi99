b3df8ea4a03529b53686e844e78936a25d8e701c
lukelin7429 <lukelin7429@users.noreply.github.com>
2026-07-25 11:32:37 +0800

    新增瑜伽師地論卷69導讀：聲聞地決擇之三
    深入釋諸毗奈耶相：制立甚深（五種制立）、說壞戒等、
    五譬喻事（含牛乳五味喻與大般涅槃經七河喻）。

--- build.py
+++ build.py
@@ -157,6 +157,7 @@ HANDMADE_PAGES=[
     "/study-group/yogacara-bhumi/vol-066/",
     "/study-group/yogacara-bhumi/vol-067/",
     "/study-group/yogacara-bhumi/vol-068/",
+    "/study-group/yogacara-bhumi/vol-069/",
     "/study-group/vimalakirti/",
     "/study-group/vimalakirti/pin01/",
     "/study-group/vimalakirti/pin02/",
