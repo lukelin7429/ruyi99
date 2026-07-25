aa6ac1abd6895aaffb5476e974b2380877ae7699
lukelin7429 <lukelin7429@users.noreply.github.com>
2026-07-25 11:28:07 +0800

    新增瑜伽師地論卷68導讀：聲聞地決擇之二
    完成釋說四聖諦（滅諦：涅槃是證所顯非生所顯；道諦）；
    釋說諂誑過患、釋說麤語聲聞、開啟釋諸毗奈耶相。

--- build.py
+++ build.py
@@ -156,6 +156,7 @@ HANDMADE_PAGES=[
     "/study-group/yogacara-bhumi/vol-065/",
     "/study-group/yogacara-bhumi/vol-066/",
     "/study-group/yogacara-bhumi/vol-067/",
+    "/study-group/yogacara-bhumi/vol-068/",
     "/study-group/vimalakirti/",
     "/study-group/vimalakirti/pin01/",
     "/study-group/vimalakirti/pin02/",
